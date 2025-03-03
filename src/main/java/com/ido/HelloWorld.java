635a6a6959fe14fcaa70e63c67a00fc563db31fe
ownedbyben <benmaryuma@gmail.com>
2025-03-03 10:57:43 +0200

    added my name to HelloWorld.java

--- src/main/java/com/ido/HelloWorld.java
+++ src/main/java/com/ido/HelloWorld.java
@@ -3,9 +3,9 @@ package com.ido;
 /**
  * Hello world!
  */
- 
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World by ben!");
     }
 }
